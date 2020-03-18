59b7d9b1406eed39867fed4a90352119de7e4ce1
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-03-19 01:12:21 +1030

    initial template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header>
+        <h1 className="mt-4 mb-4">Covid 2019</h1>
       </header>
-    </div>
+
+      <p>aasdasd</p>
+
+      <footer>
+        <Row>
+          <Col className="mt-4 text-right">
+            <a href="https://github.com/renato145/covid2019" target="_black">
+              Source code
+            </a>
+          </Col>
+        </Row>
+      </footer>
+    </Container>
   );
 }
 
