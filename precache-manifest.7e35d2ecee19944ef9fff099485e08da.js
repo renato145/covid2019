fd51e6fb8d36228b5334eb7894b6ff5ec6aea8d8
Renato <renato145@hotmail.com>
2020-03-29 17:03:46 +1030

    Updates

--- precache-manifest.7e35d2ecee19944ef9fff099485e08da.js
+++ precache-manifest.7e35d2ecee19944ef9fff099485e08da.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5631fa37838e875fbc24f9846102a54b",
+    "revision": "fd069a2f9ee0e8b27513ff7bfdde803e",
     "url": "/covid2019/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid2019/static/css/2.89f067aa.chunk.css"
   },
   {
-    "revision": "c7d8cc290bf13e6d493d",
-    "url": "/covid2019/static/css/main.1891be72.chunk.css"
+    "revision": "e85065195acfeea16b82",
+    "url": "/covid2019/static/css/main.0dd455c3.chunk.css"
   },
   {
     "revision": "776f1201be8c4767731e",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid2019/static/js/2.5ca7e290.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c7d8cc290bf13e6d493d",
-    "url": "/covid2019/static/js/main.42fe3333.chunk.js"
+    "revision": "e85065195acfeea16b82",
+    "url": "/covid2019/static/js/main.b682287d.chunk.js"
   },
   {
     "revision": "c2585ebc42e7a3105d77",
